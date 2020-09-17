5b4a5283bb5d9354ce62b91f8d87d98f52aa298d
stable-staple <lipsf1337@gmail.com>
2020-09-17 12:26:03 +0300

    new animations + images + width refactoring

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 var app = new Vue({
   el: '#app',
   data: {
-    scrollPosition: null
+    swipeDoctors: null,
+    scrollPosition: null,
+    hideDoctors: false
   },
   methods: {
-  	updateScroll () {
+  	updateScroll: function () {
   		this.scrollPosition = window.scrollY;
-  	}
+  	},
+    nextSlide: function () {
+      if (this.swipeDoctors.isBeginning && !this.hideDoctors)
+        this.hideDoctors = true;
+      else
+        this.swipeDoctors.slideNext();
+    },
+    prevSlide: function () {
+      if (this.swipeDoctors.isBeginning && this.hideDoctors)
+        this.hideDoctors = false;
+      else
+        this.swipeDoctors.slidePrev();
+    }
   },
   mounted: function () {
   	window.addEventListener('scroll', this.updateScroll);
+    this.swipeDoctors = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      speed: 400,
+      slidesOffsetBefore: 30
+    });
   }
 });
 
-var swipeDoctors = new Swiper('.swiper-container', {
-    slidesPerView: 'auto',
-    speed: 400
-});
